3ccecc0f5407ad69d62549148118d9b6efdc0468
Shayan Namayandeh <namayandeshayan@gmail.com>
2026-08-06 14:35:40 +0330

    fix logo app

---
+++ android/app/src/main/kotlin/com/amutapp/cargo/amutbar_cargo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.amutapp.cargo.amutbar_cargo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/amutapp/cargo/amutbar_cargo/amutbar_cargo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.amutapp.cargo.amutbar_cargo.amutbar_cargo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
